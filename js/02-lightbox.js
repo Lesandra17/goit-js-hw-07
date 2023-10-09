39638a45774e34e510797c801d0d3fa6f0cfccca
Lesandra17 <Lesya.lisa1708@gmail.com>
2023-10-09 21:45:54 +0300

    summary js 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description}) => `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+    </li>`).join('')
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: 'alt',
+    enableKeyboard: true,
+    captionPosition: 'bottom',
+    close: false
+})
